18467eea8f96f7eee64ecfaf985309488c67000c
Pittinan Saeyang <pittinansaeyang@gmail.com>
2024-06-24 18:22:28 +0700

    cube3d work

--- 3d/main.js
+++ 3d/main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three'
+import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -8,12 +8,12 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const geometry = new THREE.BoxGeometry( 3, 3, 1 );
+const material = new THREE.MeshBasicMaterial( { color: '#FFD5FD' } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+camera.position.z = 9;
 
 function animate() {
 
